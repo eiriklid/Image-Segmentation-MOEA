b39ae36d83858a668877cee85a2a9ac08de8870b
sverrevr <sverrevr@stud.ntnu.no>
2017-03-14 17:31:08 +0100

    Fullført NSGA-II, ikke testet

--- Image Segmentation MOEA/NSGA-II.cpp
+++ Image Segmentation MOEA/NSGA-II.cpp
@@ -1,39 +1,13 @@
 #include <vector>
 #include "Parameters.h"
 #include <iostream>
-#include "Solution.h"
 #include <stdlib.h>
 #include <time.h>
 #include <algorithm>
 
+#include "NSGA_II.h"
+
 using namespace std;
-
-
-struct Individual {
-	Solution sol;
-	int rank;
-	double	crowdingDistance;
-	Individual() { sol = Solution(); }
-	bool operator<(const Individual& rhs) {
-		if (rank != rhs.rank) return rank < rhs.rank;
-		else return crowdingDistance > rhs.crowdingDistance;
-	}
-};
-
-//Kjør samme parent child ting som jeg gjorde sist gang. 
-//Sorter etter hvilken front de er i, 
-//fronten som blir kuttet der børvi sortere etter niching strategi
-
-//Selection: Tourney
-//Vinner om man har bedre rank
-//Vinner om man har bedre crowding distance ved samme rank.
-
-//Crowding distance: Ytterpunktene på en front har uendelig
-//Ellers. Finn det nærmeste elementet på hver front
-//i_cd = sum over all fitness functions: (f[i+1]-f[i-1])/(f_max-f_min)
-//Så skaler dem utifra min og maks verdi langs paretor fronte til den fitnessen
-
-//Så basicly skal vi sortere poppulasjonen med foreldre og barn med hensyn på rank og så diversitet. 
 
 Solution NSGA_II() {
 	srand(time(NULL));
@@ -41,7 +15,9 @@ Solution NSGA_II() {
 	vector<Individual> poppulation(2 * POPPULATION_SIZE, Individual());
 	
 
-	//Beregn rank ogdiversitet
+	//Beregn rank og diversitet
+	calcRank(poppulation);
+	calcCrowdingDistance(poppulation);
 
 	//Variabler til loopen
 	Individual* parent1;
@@ -64,16 +40,32 @@ Solution NSGA_II() {
 		}
 
 		//Beregn rank og diversitet
+		calcRank(poppulation);
+		calcCrowdingDistance(poppulation);
 
 		//Sorter mhp rank så diversitet.
-		sort(poppulation.begin(), poppulation.end());
-
-		
+		sort(poppulation.begin(), poppulation.end());		
 	}
 }
 
-Individual* tourney(const vector<Individual>* poppulation) {
+Individual* tourney(vector<Individual>* poppulation) {
+	Individual* winner = &(*poppulation)[rand() % POPPULATION_SIZE];
 
+	//sjangse for at ikke tilfeldig velges
+	if (!TOURNEY_ERROR_RATE || (rand() % TOURNEY_ERROR_RATE)) {
+
+		vector<int> participants(TOURNEY_SIZE, 0);
+		for (int i = 0; i < TOURNEY_SIZE; ++i) {
+			participants[i] = rand() % POPPULATION_SIZE;
+		}
+
+		for (int i = 0; i < TOURNEY_SIZE; ++i) {
+			if ((*poppulation)[participants[i]] < *winner) {
+				winner = &(*poppulation)[participants[i]];
+			}
+		}
+	}
+	return winner;
 }
 
 void crossover(const Individual* parent1, const Individual* parent2, Individual* child) {
@@ -82,4 +74,93 @@ void crossover(const Individual* parent1, const Individual* parent2, Individual*
 
 void mutate(Individual* ind) {
 
+}
+
+void calcRank(vector<Individual>& poppulation) {
+	for (int i = 0; i < poppulation.size(); ++i) {
+		poppulation[i].domminatingSolutionIDs.clear(); //HEAVY
+		//Find all dominating individuals
+		for (int j = 0; j < poppulation.size(); ++j) {
+			if (i != j) {
+				if (poppulation[j].sol.fitness[0] < poppulation[i].sol.fitness[0]
+					&& poppulation[j].sol.fitness[1] < poppulation[i].sol.fitness[1]
+					&& poppulation[j].sol.fitness[2] < poppulation[i].sol.fitness[2]) {
+					poppulation[i].domminatingSolutionIDs.push_back(j);
+				}
+			}
+		}
+	}
+
+	//Sort based on dominating solution size.
+	sort(poppulation.begin(), poppulation.end(), sortOnDominatingSetSize);
+
+	for (auto it = poppulation.begin(); it != poppulation.end(); ++it) {
+		if (it->domminatingSolutionIDs.size() == 0) {
+			it->rank = 0;
+		}
+		else {
+			//rank = highest rank of dominators +1
+			it->rank = INT_MAX;
+			for (auto dt = it->domminatingSolutionIDs.begin(); dt != it->domminatingSolutionIDs.end(); ++dt) {
+				it->rank = min(it->rank, poppulation[*dt].rank+1);
+			}
+		}
+	}
+}
+
+bool sortOnDominatingSetSize(const Individual& lhs, const Individual& rhs) {
+	return lhs.domminatingSolutionIDs.size() < rhs.domminatingSolutionIDs.size();
+}
+
+
+//Calcullate rank first
+void calcCrowdingDistance(vector<Individual>& poppulation) {
+	//Må sette alle crowding distansene til 0 her!!
+	for (auto it = poppulation.begin(); it != poppulation.end(); ++it) {
+		it->crowdingDistance = 0;
+	}
+
+	int rankEnd = 0;
+	double f_min;
+	double f_max;
+	typedef bool(*sortFunc)(const Individual&, const Individual&);
+	sortFunc sortArray[3];
+	sortArray[0] = &sortOnFitness1;
+	sortArray[1] = &sortOnFitness2;
+	sortArray[2] = &sortOnFitness3;
+	for (int rankStart = 0; rankStart < poppulation.size();) {
+		//find rank end
+		while (rankEnd != poppulation.size() && poppulation[rankEnd].rank == poppulation[rankStart].rank) {
+			rankEnd++;
+		}
+
+
+		for (int objFunc = 0; objFunc < NUM_OBJECTIVES; ++objFunc) {
+			//sorter [rankStart, rankEnd> mhp hver av fitness funksjonene
+			sort(poppulation.begin() + rankStart, poppulation.end() + rankEnd, sortArray[objFunc]);
+			//Setter ytterpunktene i denne dimmensjonen til å ha uendelig crowdingDistance 
+			poppulation[rankStart].crowdingDistance = INFINITY;
+			poppulation[rankEnd - 1].crowdingDistance = INFINITY;
+			f_min = poppulation[rankStart].sol.fitness[objFunc];
+			f_max = poppulation[rankEnd - 1].sol.fitness[objFunc];
+
+			if (rankStart + 1 < rankEnd - 1) {
+				//mer enn 2 elementer
+				for (auto it = poppulation.begin() + rankStart + 1; it != poppulation.begin() + rankEnd - 1; ++it) {
+					it->crowdingDistance += ((it + 1)->sol.fitness[objFunc] - (it - 1)->sol.fitness[objFunc]) / (f_max - f_min);
+				}
+			}
+		}
+		rankStart = rankEnd;
+	}
+}
+
+bool sortOnFitness1(const Individual& lhs, const Individual& rhs) {
+	return lhs.sol.fitness[0] < lhs.sol.fitness[0];
+}
+bool sortOnFitness2(const Individual& lhs, const Individual& rhs) {
+	return lhs.sol.fitness[1] < lhs.sol.fitness[1];
+}
+bool sortOnFitness3(const Individual& lhs, const Individual& rhs) {
+	return lhs.sol.fitness[2] < lhs.sol.fitness[2];
 }
--- Image Segmentation MOEA/Parameters.h
+++ Image Segmentation MOEA/Parameters.h
@@ -5,6 +5,6 @@
 //n betyr 1/n. n==0 betyr probabiliy = 0
 #define CLONE_RATE 10
 
-
 //NSGA-II
 #define POPPULATION_SIZE 100
+#define TOURNEY_SIZE 2
