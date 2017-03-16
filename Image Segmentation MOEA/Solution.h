d4402fd6848927bdca7e4a8311ccc816cdb2490c
sverrevr <sverrevr@stud.ntnu.no>
2017-03-16 09:06:39 +0100

    La til solution.h mal

--- Image Segmentation MOEA/Solution.h
+++ Image Segmentation MOEA/Solution.h
@@ -3,13 +3,18 @@
 #include <vector>
 
 #include "Segment.h"
-class Solution
-{
+class Solution{
 private:
 	std::vector<Segment> segments;
 
 
 public:
 	Solution();
+	double f1();	// Overal deviation
+	double f2();	// Edge
+	double f3();	// Connectivity measure
+	void fitness(double* val1, double* val2, double* val3);
+
+	double fitness[3];
 };
 
